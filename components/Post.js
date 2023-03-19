50ff46375dab122569010baa488068b0f8823aba
unknown <harizsharizad@gmail.com>
2023-03-19 11:41:15 +0800

    create the header and image sections of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {DotsHorizontalIcon} from "@heroicons/react/outline";
 
 function Post({ img, userImg, caption, username, id }) {
   return (
-    <div>Post</div>
+    <div className="bg-white my-7 border rounded-md">
+        {/* Post Header */}
+
+        <div className="flex items-center p-5">
+            <img 
+            className="h-12 rounded-full object-cover border p-1 mr-3"
+            src={userImg} 
+            alt={username} />
+            <p className="font-bold flex-1">{username}</p>
+            <DotsHorizontalIcon className="h-5" />
+        </div>
+
+        {/* Post Image */}
+
+        <img className="object-cover w-full" src={img} alt="" />
+
+    </div>
   )
 }
 
