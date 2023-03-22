2dea0bb7d09d55de53729457e9fc2063486ba2a3
unknown <harizsharizad@gmail.com>
2023-03-22 15:36:05 +0800

    fix bug 3.36pm

--- components/Post.js
+++ components/Post.js
@@ -114,19 +114,19 @@ export default function Post({ img, userImg, caption, username, id }) {
         {caption}
       </p>
       {comments.length > 0 && (
-        <div key={comment.id} className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+        <div key={comment.data().id} className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
           {comments.map((comment) => (
-            <div className="flex items-center space-x-2 mb-2">
-              <img
-                className="h-7  rounded-full object-cover"
-                src={comment.data().userImage}
-                alt="user-image"
-              />
-              <p className="font-semibold">{comment.data().username}</p>
-              <p className="flex-1 truncate">{comment.data().comment}</p>
-              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
-            </div>
-          ))}
+  <div key={comment.id} className="flex items-center space-x-2 mb-2">
+    <img
+      className="h-7  rounded-full object-cover"
+      src={comment.data().userImage}
+      alt="user-image"
+    />
+    <p className="font-semibold">{comment.data().username}</p>
+    <p className="flex-1 truncate">{comment.data().comment}</p>
+    <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+  </div>
+))}
         </div>
       )}
 
